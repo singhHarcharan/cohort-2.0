64718c8125b5e8d064f2aae97adda386f04d7c28
singhHarcharan <gurcharansinghkhalsa262@gmail.com>
2024-08-24 17:15:52 +0530

    express

---
+++ express-class/index.js
@@ -0,0 +1,9 @@
+        try {
+            let a;
+            console.log(a.length);
+            console.log("Hi there from try statement");
+        } catch(e) {
+            console.log("Hi there from catch statement");
+        }
+
+        console.log("Hi there");
